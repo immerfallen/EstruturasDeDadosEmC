ed998c33411818d192200748b6f749b481205a40
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-11-23 20:58:56 +0000

    Structs

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+struct pessoa
+{
+	int idade;
+	float altura;
+};
+
+typedef struct pessoa Pessoa;
+
+int main() {
+
+	Pessoa p;
+
+	p.idade =5;
+	p.altura = 1.78;
+
+	printf("a altura da pessoa é %.2f e a idade da pessoa é %d", p.altura, p.idade);
+  
   return 0;
 }
