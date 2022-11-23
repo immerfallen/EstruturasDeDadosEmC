78d51c81a3ffa7092b52ba99c675d26d05060008
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-11-23 21:04:09 +0000

    Ponteiros

--- main.c
+++ main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
-struct pessoa
+/*struct pessoa
 {
 	int idade;
 	float altura;
 };
 
+
 typedef struct pessoa Pessoa;
+*/
+
+int *p;
+
+int val = 5;
 
 int main() {
-
+/*
 	Pessoa p;
 
 	p.idade =5;
 	p.altura = 1.78;
 
 	printf("a altura da pessoa é %.2f e a idade da pessoa é %d", p.altura, p.idade);
-  
+  */
+
+ p = &val;
+
+	printf("O valor apontado por p é %d", *p );
+
+	
   return 0;
 }
